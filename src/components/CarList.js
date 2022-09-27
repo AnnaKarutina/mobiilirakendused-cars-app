28d8969f755798be77a5797f183879d7d85badc6
Anna at Mac class <anna.karutina@voco.ee>
2022-09-27 13:18:52 +0300

    Use data inside component

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,24 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text } from "react-native";
 import axios from 'axios';
 
+const CarList = () => {
+  const [cars, getCars] = useState('');
 
-class CarList extends Component {
-  componentDidMount() {
-    console.log('console output from car components');
+  useEffect(() => {
+    getAllCars();
+  }, []);
+
+  const getAllCars = () => {
     axios.get('https://givecars.herokuapp.com')
       .then(response => {
-      console.log(response.data);
-    });
+        const allCars = response.data;
+        getCars(allCars);
+    })
+      .catch(error => console.log(`Error: ${error}`));
   }
 
-  render() {
-    return (
-      <View>
-        <Text>Car List</Text>
-      </View>
-    );
-  }
-}
+  console.log(cars)
+
+  const renderList = () => {
+    return cars.map(brand => {
+      return <Text key={brand.model[0].name}>{brand.model[0].name}</Text>;
+    });
+  };
+
+  return (
+    <View>
+      <Text>{renderList()}</Text>
+    </View>
+  );
+};
 
 export default CarList;
