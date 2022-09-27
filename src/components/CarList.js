774bcf9c75a78630bd68ef5f8d42e8c198b7c20d
Anna at Mac class <anna.karutina@voco.ee>
2022-09-27 12:31:29 +0300

    Get data from API

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import { View, Text } from "react-native";
+import axios from 'axios';
 
 
 class CarList extends Component {
   componentDidMount() {
-    console.log('console output from car components')
+    console.log('console output from car components');
+    axios.get('https://givecars.herokuapp.com')
+      .then(response => {
+      console.log(response.data);
+    });
   }
 
   render() {
