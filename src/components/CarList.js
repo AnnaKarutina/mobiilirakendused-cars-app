746b53cde5a3d9f6a1861929fa70fccc3ab7ad6e
Anna at Mac class <anna.karutina@voco.ee>
2022-09-27 12:18:30 +0300

    Add componentDidMount to CarList for data fetching

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,6 +3,10 @@ import { View, Text } from "react-native";
 
 
 class CarList extends Component {
+  componentDidMount() {
+    console.log('console output from car components')
+  }
+
   render() {
     return (
       <View>
